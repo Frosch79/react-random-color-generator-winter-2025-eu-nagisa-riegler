4fc537cdeec9747ba22fe41a205289ae506a081c
Frosch79 <frosch.79.photo@gmail.com>
2025-03-24 18:00:29 +0100

    Change code of colortext component

--- src/ColorText.js
+++ src/ColorText.js
@@ -1,7 +1,7 @@
 export default function ColorText(props) {
   return (
-    <p className={props.className} style={props.styleColor}>
+    <div className={props.className} style={props.styleColor}>
       {'Generated Color: ' + props.color}
-    </p>
+    </div>
   );
 }
